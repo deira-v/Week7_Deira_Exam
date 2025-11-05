ee5a33aba29d65c658be75bc2c160dc32a994396
DeiraValci-STUDENT <deiravalci@gmail.com>
2025-11-05 16:31:06 +0000

    Product Class created

--- src/main/java/ie/atu/ProductMain.java
+++ src/main/java/ie/atu/ProductMain.java
@@ -3,6 +3,12 @@ package ie.atu;
 import java.util.Scanner;
 
 public class ProductMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+
+
+
+    }
 
 }
