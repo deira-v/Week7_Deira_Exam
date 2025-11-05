5e1189b2c185d58917849f3dd238c7bed6cfb845
DeiraValci-STUDENT <deiravalci@gmail.com>
2025-11-05 16:36:07 +0000

    Product Class created

--- src/main/java/ie/atu/Product.java
+++ src/main/java/ie/atu/Product.java
@@ -1,6 +1,53 @@
 package ie.atu;
 
 public class Product {
+    //fields of Product
+    private String productID;
+    private String name;
+    private double price;
+    private int quantity;
 
+    //Constructor
+    public Product(){
+        productID = "";
+        name = "";
+        price = 0;
+        quantity = 0;
+    }
 
+    public String getProductID() {
+        return productID;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productID='" + productID + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
